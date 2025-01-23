ff747c415ac1fbc4cdff9e5b5c40a49e0f0dae7a
danielkoryat <dk@sidekickwellness.com>
2025-01-23 13:53:37 -0500

    change the use from classes to interfaces in entity representation

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { initializeApp } from "firebase/app";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -16,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export { auth };
