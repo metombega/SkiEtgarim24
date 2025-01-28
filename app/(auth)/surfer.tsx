18abb228d78b44c2a182d0d5755bf24c70ffbd23
metombega <metombega@gmail.com>
2025-01-27 21:23:33 -0500

    added changes after meeting

--- app/(auth)/surfer.tsx
+++ app/(auth)/surfer.tsx
@@ -1,36 +1,15 @@
 import React, { useState } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
-import { Calendar, DateData } from 'react-native-calendars';
 import { useRouter } from "expo-router";
+import CustomCalendar from '@/components/CustomCalendar';
 
 export default function Surfer() {
   const router = useRouter();
   const [selectedDay, setSelectedDay] = useState<string | null>(null);
-    // Define marked dates
-    const markedDates: Record<string, { selected: boolean; selectedColor: string }> = {
-      '2025-01-25': { selected: true, selectedColor: 'green' }, // Selectable
-      '2025-01-26': { selected: true, selectedColor: 'red' },   // Taken
-      '2025-01-27': { selected: true, selectedColor: 'black' }, // Unavailable
-      '2025-01-28': { selected: true, selectedColor: 'blue' },  // Already signed
-    };
-  
-    const handleDayPress = (day: DateData) => {
-      if (markedDates[day.dateString]?.selectedColor === 'green') {
-        setSelectedDay(day.dateString);
-      } else {
-        setSelectedDay(null);
-      }
-    };
-  
+
     return (
       <View style={styles.container}>
-        <Calendar
-          onDayPress={handleDayPress}
-          markedDates={{
-            ...markedDates,
-            ...(selectedDay ? { [selectedDay]: { selected: true, selectedColor: 'green', marked: true } } : {}),
-          }}
-        />
+        <CustomCalendar setSelectedDay={setSelectedDay}/>
         <Button
           title="Sign To Selected Day"
           onPress={() => alert(`Signed up for ${selectedDay}`)}
