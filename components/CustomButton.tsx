332935711cd150b4326065904cf5bb1c1935c48e
metombega <metombega@gmail.com>
2025-02-13 18:09:40 -0500

    Added admin page

---
+++ components/CustomButton.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Colors } from '@/app/config/constants/constants';
+
+interface CustomButtonProps {
+  title: string;
+  description: string;
+  imageSource: any;
+  onPress: () => void;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({ title, description, imageSource, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Image source={imageSource} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: Colors.light_orange,
+    borderRadius: 5,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
+
+export default CustomButton;
