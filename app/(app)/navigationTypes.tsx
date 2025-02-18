922f5371cd49dde183241ea6013274495640c035
metombega <metombega@gmail.com>
2025-02-18 10:19:28 -0500

    added volunteer details page

--- app/(app)/navigationTypes.tsx
+++ app/(app)/navigationTypes.tsx
@@ -4,7 +4,11 @@ import { StackNavigationProp } from '@react-navigation/stack';
 export type RootStackParamList = {
   SurfersManagement: undefined;
   surfer_details: { id: string };
+  VolunteersManagement: undefined;
+  volunteer_details: { id: string };
 };
 
 export type SurferDetailsRouteProp = RouteProp<RootStackParamList, 'surfer_details'>;
 export type SurferDetailsNavigationProp = StackNavigationProp<RootStackParamList, 'surfer_details'>;
+export type VolunteerDetailsRouteProp = RouteProp<RootStackParamList, 'volunteer_details'>;
+export type VolunteerDetailsNavigationProp = StackNavigationProp<RootStackParamList, 'volunteer_details'>;
