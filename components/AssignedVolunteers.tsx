5596db1b318809f790fad601fe1bc41128e68d8c
metombega <metombega@gmail.com>
2025-03-04 11:12:14 -0500

    step 2 and 3 look only on initialized activities

--- components/AssignedVolunteers.tsx
+++ components/AssignedVolunteers.tsx
@@ -41,6 +41,7 @@ const AssignedVolunteers: React.FC<AssignedVolunteersProps> = ({ onSave }) => {
       const newVolunteers: Set<string> = new Set();
 
       for (const date in fetchedData) {
+        if (fetchedData[date].status !== "initialized") continue;
         newDates.push(date);
         const assignedVolunteers = Array.isArray(fetchedData[date].volunteers)
           ? fetchedData[date].volunteers
